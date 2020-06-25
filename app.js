a725a7f3007da622cce3b600e52006e5d73ee80d
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-06-25 14:18:15 +0300

    Working with Readable && Writable Streams

--- app.js
+++ app.js
@@ -1,46 +1,7 @@
 const fs = require('fs');
-
-// fs.mkdir('tutorial', (err) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         fs.writeFile('./tutorial/example.txt', '123', (err) => {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 console.log('Dir & file created');
-//             }
-//         })
-//     }
-// })
-
-// fs.unlink('./tutorial/example.txt', (err) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         fs.rmdir('tutorial', (err) => {
-//             if (err) {
-//                 console.log(err);
-//             } else {
-//                 console.log('folder & file deleted');
-//             }
-//         })
-//     }
-// })
-
-fs.readdir('folder', (err, files) => {
-    if (err) {
-        console.log(err);
-    } else {
-        for (let file of files) {
-            fs.unlink('./folder/' + file, (err) => {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('file ' + file + ' deleted');
-                }
-            })
-        }
-    }
+const readStream = fs.createReadStream('./example.txt', 'utf8');
+const writeStream = fs.createWriteStream('./example2.txt');
+readStream.on('data', (chunk) => {
+    writeStream.write(chunk);
 })
 
--- tutorial.js
+++
@@ -1,8 +0,0 @@
-const sum = (num1, num2) => num1 + num2;
-const PI = 3.14;
-class someMathObject {
-    constructor() {
-        console.log('Object created');
-    }
-}
-module.exports = {sum : sum, PI : PI, someMathObject : someMathObject}
