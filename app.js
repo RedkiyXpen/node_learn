702be093cb3a7b805630a8684558491cf326f4b5
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-07-27 22:04:24 +0900

    creating http server using http module

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const fs = require('fs'),
-      zlib = require('zlib'),
-      gunzip = zlib.createGunzip(),
-      readStream = fs.createReadStream('example1.txt.gz'),
-      writeStream = fs.createWriteStream('uncompressed.txt');
-readStream.pipe(gunzip).pipe(writeStream);
+const { resolveSoa } = require('dns');
 
+const http = require('http'),
+      server = http.createServer((req, res) => {
+        if (req === '/') {
+            res.write('hello world from nodejs');
+            res.end();
+        } else {
+            res.write('use some other domain');
+            res.end();
+        }
+      });
+
+server.listen('3000');
