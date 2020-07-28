adf544dc70a027090fdaf741df28f4862b3fdb3f
Nikolai Chirkov <qwertymonstro@gmail.com>
2020-07-28 18:48:38 +0900

    create package.json using npm init

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http'),
       fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    const readStream = fs.createReadStream('./static/example.JPG');
-    res.writeHead(200, {'Content-type': 'image/jpeg'});
+    const readStream = fs.createReadStream('./static/example.json');
+    res.writeHead(200, {'Content-type': 'application/json'});
     readStream.pipe(res);
 }).listen('3000');
